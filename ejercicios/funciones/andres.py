45e0af333a2fd9f52466ef16858a80168399edad
carolincamacho <carolincamacho@javerianacali.edu.co>
2025-04-15 20:05:12 -0500

    clase_Abril_15

--- ejercicios/funciones/andres.py
+++ ejercicios/funciones/andres.py
@@ -1,4 +1,2 @@
 # Ejercicio 2: Intersección de Listas
 # Escribir una función que reciba dos listas y devuelva una nueva lista con los elementos comunes entre ambas (sin repe
-
-while intento_key !int (input(ingrese))
--- ejercicios/funciones/cristian.py
+++ ejercicios/funciones/cristian.py
@@ -1,19 +1,51 @@
-# Ejercicio 2: Intersección de Listas
-# Escribir una función que reciba dos listas y devuelva una nueva lista con los elementos
-#  comunes entre ambas (sin repetir).
-
-def listas(lista_1,lista_2):
-  
-    
-    return lista_1,lista_2
-
-print(listas(["perro, foca, caballo"], ["ballena, foca, tiburon"]))
-
+# # Ejercicio 2: Intersección de Listas
+# # Escribir una función que reciba dos listas y devuelva una nueva lista con los elementos
+# #  comunes entre ambas (sin repetir).
 
 # def listas(lista_1,lista_2):
-#     lista_1="perro, foca, caballo"
-#     lista_2="ballena, foca, tiburon"
-#     resultado=lista_1 + " " + lista_2
+  
+    
 #     return lista_1,lista_2
 
 # print(listas(["perro, foca, caballo"], ["ballena, foca, tiburon"]))
+
+
+# # def listas(lista_1,lista_2):
+# #     lista_1="perro, foca, caballo"
+# #     lista_2="ballena, foca, tiburon"
+# #     resultado=lista_1 + " " + lista_2
+# #     return lista_1,lista_2
+
+# # print(listas(["perro, foca, caballo"], ["ballena, foca, tiburon"]))
+
+
+
+
+empleados={
+    "carolina":74,
+    "cristian":32,
+    "julian":12,
+    "Andres":6,
+    "guille":50
+}
+
+#edades ={}
+
+def clasificar (diccionario):
+    clasificados={"niños":{},"adultos":{},"mayores":{}}
+    for clave,valor in diccionario.items():
+        if valor<13:
+            #edades.update({"niños":{clave:valor}})
+            #edades[clave]=valor
+            clasificados["niños"][clave]=valor
+        elif valor>60:
+            #edades.update({"Mayores":{clave:valor}})
+            #edades[clave]=valor
+            clasificados["mayores"][clave]=valor
+        else:
+            #edades.update({"Adultos":{clave:valor}})
+            #edades[clave]=valor
+            clasificados["adultos"][clave]=valor
+        
+    return clasificados
+print(clasificar(empleados))
